5a682e128e643297b74c0530c9fcf69676ee64be
Yaren <53363650+YARENP@users.noreply.github.com>
2022-06-24 16:18:51 +0300

    customer model is updated

--- microservices/customer/src/main/java/com/microservice/customer/Customer.java
+++ microservices/customer/src/main/java/com/microservice/customer/Customer.java
@@ -1,13 +1,30 @@
 package com.microservice.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 // customer is the model of this microservice
 // to tell the spring this is a model we have to annotate as follows
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+
     private String firstName;
     private String lastName;
     private String email;
