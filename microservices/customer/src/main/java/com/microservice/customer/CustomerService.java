cf0c54e3231c5ec415e9894bb5935eb597f0d4c4
Yaren <53363650+YARENP@users.noreply.github.com>
2022-06-24 16:21:40 +0300

    Repository interface added

--- microservices/customer/src/main/java/com/microservice/customer/CustomerService.java
+++ microservices/customer/src/main/java/com/microservice/customer/CustomerService.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Service;
 
 // this is a spring service therefore we have to annotate with service keyword
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
 
         //todo: validate emial lastname and firstname
-        //todo: save the customer into database
+        customerRepository.save(customer);
     }
 }
