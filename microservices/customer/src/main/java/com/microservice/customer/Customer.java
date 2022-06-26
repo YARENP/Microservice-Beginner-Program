8f236cdfe75b9a032bf48a91af01974f7c414a63
Yaren <53363650+YARENP@users.noreply.github.com>
2022-06-27 02:37:38 +0300

    create customer tables on postgre sql

--- microservices/customer/src/main/java/com/microservice/customer/Customer.java
+++ microservices/customer/src/main/java/com/microservice/customer/Customer.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -15,19 +16,23 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 public class Customer {
+    //    @Id
+//    @SequenceGenerator(
+//            name = "customer_id_sequence",
+//            sequenceName = "customer_id_sequence"
+//    )
+//    @GeneratedValue(
+//            strategy = GenerationType.SEQUENCE,
+//            generator = "customer_id_sequence"
+//    )
     @Id
-    @SequenceGenerator(
-            name = "customer_id_sequence",
-            sequenceName = "customer_id_sequence"
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "customer_id_sequence"
-    )
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String Id;
 
     private String firstName;
     private String lastName;
     private String email;
-    private Integer id;
+    // private Integer id;
 
 }
